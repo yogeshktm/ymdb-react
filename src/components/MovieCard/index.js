b4a17cd4c1c644384c1fc6b3b7811cbef5815796
yogesh.ktm <yogesh.ktm@gmail.com>
2021-02-02 21:01:45 +0530

    Type of search added and other changes

--- src/components/MovieCard/index.js
+++ src/components/MovieCard/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
-
 export class MovieCard extends React.Component{
     render(){
-        const {Title,Poster,Year,Actors,Plot,Language,Genre,Director,Runtime,Country} = this.props.fullData;
+        const {imdbID,totalSeasons,imdbRating,Title,Poster,Year,Actors,Plot,Language,Genre,Director,Runtime,Country} = this.props.fullData;
+        let imdbLink = 'https://imdb.com/title/' + imdbID;
         return(
           <>
           <form onSubmit={this.props.handleSubmit} className="search-box">
-           <input autoFocus className="search" type="search" id="search" autoComplete="off" onChange={this.props.handleChange} placeholder="Search"/>
+          <input autoFocus className="search" type="search" id="search" autoComplete="off" onChange={this.props.handleChange} placeholder="Search"/>
+          <select onChange={this.props.handleMovieTypeChange}>
+            <option>movie</option>
+            <option>series</option>
+            <option>episodes</option>
+          </select>
           <button className="btn" type="submit">SEARCH</button>
          </form>
-          <div class="card">
-            <div class="movie-info">
+        <div class="card">
+          <div class={ this.props.notFound === true ? 'hidden' : 'movie-info' }>
               <p><span className="release-year">{Year}</span> - <span className="country">{Country}</span></p>
               <h2 className="movie-title">{Title}</h2>
               <p class="lang">Language: {Language}</p>
@@ -20,9 +25,11 @@ export class MovieCard extends React.Component{
               <p>Cast: {Actors}</p>
               <p>Plot:</p>
               <p>{Plot}</p>
-             </div>
-             <img class="poster-img" src={Poster} alt={Title}/>
-          </div>
+              <p>IMDB Rating : {imdbRating} / 10</p>
+              <a href={imdbLink}>IMDB Link</a>
+              </div>
+              <img class="poster-img" src={Poster === 'N/A' ? `https://dummyimage.com/390x610/c89f9b/fff.png&text=Poster+not+found` : Poster} alt={Title}/>
+            </div>
           </>
         );
       }
--- src/container/MovieCardContainer/index.js
+++ src/container/MovieCardContainer/index.js
@@ -10,11 +10,15 @@ export class MovieCardContainer extends React.Component{
       released:"",
       genre:"",
       movieData:"",
-      searchKeyword:""
+      searchKeyword:"",
+      notFound: false,
+      searchType: "movie"
     };
     this.getMovieDetails = this.getMovieDetails.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.handleResponse = this.handleResponse.bind(this);
+    this.handleMovieTypeChange = this.handleMovieTypeChange.bind(this);
   }
   componentWillMount(){
 
@@ -24,25 +28,42 @@ export class MovieCardContainer extends React.Component{
     this.getMovieDetails(defaultMovie);
   }
   getMovieDetails(movieName){
-    const apiUrl = "https://www.omdbapi.com/?";
+    const apiUrl = "http://www.omdbapi.com/?";
     const apiKey = "7c5d79b";
-    const finalUrl = apiUrl + 'apiKey=' + apiKey + '&t=' + movieName + '&plot=full';
-    let response;
+    const searchType = this.state.searchType;
+    const finalUrl = apiUrl + 'apiKey=' + apiKey + '&t=' + movieName + '&type=' +searchType + '&plot=full';
     fetch(finalUrl)
         .then(response => response.json())
+        .then(this.handleResponse)
         .then(
             // data => console.log(data),
             data => this.setState({
                 movieData:data
             })
-        );
+        )
+        .catch(error => console.log(error) );
+
+  }
+  handleResponse (response){
+    if (response.Response === "False") {
+      console.log("inside")
+      this.setState({
+        notFound:true
+      })
+    }
+    else{
+      this.setState({
+        notFound:false
+      })
+    }
+  return response;
   }
   handleSubmit(e){
     e.preventDefault();
     let searchKeyword = this.state.searchKeyword;
     console.log(searchKeyword.length)
     if(searchKeyword.length > 0){
-      this.getMovieDetails(this.state.searchKeyword);
+      this.getMovieDetails(this.state.searchKeyword,this.state.searchType);
     }
   }
   handleChange(e){
@@ -51,10 +72,24 @@ export class MovieCardContainer extends React.Component{
       searchKeyword:e.target.value
     })
   }
+  handleMovieTypeChange(e){
+    console.log(e.target.value);
+    this.setState({
+      searchType:e.target.value
+    })
+  }
   render(){
       return(
         <div class="container">
-          <MovieCard handleChange={this.handleChange} handleSubmit={this.handleSubmit} fullData={this.state.movieData}></MovieCard>
+          <MovieCard notFound={this.state.notFound} 
+          handleChange={this.handleChange} 
+          handleSubmit={this.handleSubmit}
+          handleMovieTypeChange={this.handleMovieTypeChange} 
+          fullData={this.state.movieData}>
+          </MovieCard>
+          {
+            this.state.notFound ? <p>Movie not found</p> : null
+          }
         </div>
       )
     }
